3aacd3ed913ef097178627c7a194b9958ae64e85
airinsx <134726589+airinsx@users.noreply.github.com>
2025-03-02 21:30:34 +0100

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -2,6 +2,7 @@ const admin = require("firebase-admin");
 const { getFirestore } = require("firebase-admin/firestore");
 const path = require("path");
 
+// Load service account JSON
 const serviceAccount = require(path.join(__dirname, "serviceAccount.json"));
 
 admin.initializeApp({
